f1cbee5f003e7a1fa5f206c2d75f35571d3e6520
Kavin GG <kevinhz2022@gmail.com>
2023-02-06 23:41:02 -0600

    Mostrando status short

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log(saludar);
 saludar = Saludo("Hi, I am Kavin");
 console.log(saludar);
 
+saludar = Saludo("I still remember, thirf of december")
 
 function Saludo(saludo) {
   return saludo;
